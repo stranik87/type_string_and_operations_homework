e198422393fafda71bb97fec96635fa7efdb9f3d
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-09 07:22:34 +0300

    str09

--- String09.py
+++ String09.py
@@ -8,4 +8,4 @@ def main(x1,x2,x3):
     Returns:
         str: return answer.
     """
-    return  "[" + "x1" + "," + "x2" +  "," + "x3" + "]"
+    return '"' + "[" + "x1" + "," + "x2" +  "," + "x3" + "]" + '"'
